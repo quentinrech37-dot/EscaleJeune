dc6930145abc3dc9b5423c7f070b6941bbe15b55
Quentin Rech <votre.email@exemple.com>
2026-03-03 13:40:13 +0100

    Mises à jour site Escale Jeunes

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -4,7 +4,7 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
 
 // 2) Votre SW de cache (copiez-collez ici le contenu actuel de sw.js)
-const CACHE = "escale-v132";
+const CACHE = "escale-v133";
 
 const ASSETS = [
   "/",
